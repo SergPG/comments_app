2e280547bbb36406605f82177357564c463d6345
SergPG <serg-popov@ukr.net>
2026-05-17 11:11:57 +0300

    WIP: Add Meilisearch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+    @users = User.all
+
+    @comments = fetch_comments
+
+    @notifications = current_user.received_notifications.unread
+  end
+
+  private
+
+  def fetch_comments
+    if params[:q].present?
+      Comment.search(params[:q], sort: ["created_at:desc"])
+    else
+      Comment.includes(:user).order(created_at: :desc)
+    end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,19 @@
 class Comment < ApplicationRecord
+  include Meilisearch::Rails
+
   belongs_to :user
 
   validates :body, presence: true
 
   after_create_commit :create_mentions_notifications
+
+  meilisearch do
+    attribute :body
+    attribute :created_at
+
+    searchable_attributes [ :body ]
+    sortable_attributes [ :created_at ]
+  end
 
   private
 
